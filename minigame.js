173fdd6b13d6b1774abbd9c707d50370d6f02f2f
Lavya Bansal <lavyabansal16@gmail.com>
2022-09-23 15:10:10 +1000

    further updates

--- minigame.js
+++ minigame.js
@@ -1,22 +1,20 @@
 // Computed Properties of Vue
 new Vue({
-    el: '#vue-app',
+    el: '#app',
     data:{
         a:0,
-        b:0
+        b:0,
     }
 },
 );
 
-Vue.filter('to-uppercase',function(value){
-    return value.toUpperCase(
-
-    )
-    });
+Vue.filter('to-uppercase', function(value){
+    return value.toUpperCase()
+});
 
 new Vue({
-    el:'#app',
-    data:{
-        message:"Hope"
-    }
+    el: '#vue-app',
+    data: {
+        message: 'hope you enjoy'
+    },
 })
